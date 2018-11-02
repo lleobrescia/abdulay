bb265362924431520d15531050d4d67a4c8e5d4e
Leo Brescia <leonardo@leobrescia.com.br>
2018-11-02 20:02:14 -0300

    chore: remove unnecessary files and functions
    remove scss shop folder
    remove sidebar
    remove widget
    
    this theme will not use these features

--- wp-content/themes/abdulay/404.php
+++ wp-content/themes/abdulay/404.php
@@ -21,12 +21,6 @@ get_header();
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'abdulay' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
 					<div class="widget widget_categories">
 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'abdulay' ); ?></h2>
 						<ul>
@@ -45,9 +39,6 @@ get_header();
 					<?php
 					/* translators: %1$s: smiley */
 					$abdulay_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'abdulay' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$abdulay_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
