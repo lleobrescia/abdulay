4870a53c1e94fc2bbcad8ed2f3fb1625ba5c7af7
Leo Brescia <leobrescia@ioasys.com.br>
2018-11-26 17:44:49 -0200

    feat: add collapse to questions

--- wp-content/themes/abdulay/template-parts/content-faq.php
+++ wp-content/themes/abdulay/template-parts/content-faq.php
@@ -10,8 +10,13 @@
 ?>
 
 <section id="faq-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php the_title( '<h3 class="faq__title">', '</h3>' ); ?>
-  <div class="faq__content">
-    <?php the_field('digite_a_resposta'); ?>
-  </div><!-- faq__content -->
+  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse<?php the_ID(); ?>">
+    <?php the_title( '<h3 class="faq__title">', '</h3>' ); ?>
+  </button>
+  <div id="collapse<?php the_ID(); ?>" class="collapse show" aria-labelledby="heading<?php the_ID(); ?>" >
+    <div class="faq__content">
+      <?php the_field('digite_a_resposta'); ?>
+    </div><!-- faq__content -->
+  </div>
+
 </section><!-- #post-<?php the_ID(); ?> -->
